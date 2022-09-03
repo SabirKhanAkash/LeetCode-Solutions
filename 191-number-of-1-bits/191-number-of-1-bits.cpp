208432d93553bb763d32d1877d6903d68cceca85
Sabir Khan Akash <4175.akash@gmail.com>
2022-09-03 17:00:36 +0600

    Time: 2 ms (62.88%), Space: 6.1 MB (47.61%) - LeetHub

---
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int decToBinary(uint32_t n)
+    {
+        int count = 0;
+        int binaryNum[32];
+        int i = 0;
+        while (n > 0) {
+            binaryNum[i] = n % 2;
+            n = n / 2;
+            i++;
+        }
+        for (int j = i - 1; j >= 0; j--) {
+            if(binaryNum[j] == 1)
+                count++;
+        }
+        return count;
+    }
+    int hammingWeight(uint32_t n) {
+        
+        return decToBinary(n);
+    }
+};
