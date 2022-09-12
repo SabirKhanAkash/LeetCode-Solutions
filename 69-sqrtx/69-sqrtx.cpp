388bc395b4ac1e0f14df171fac52f31c06d385cb
Sabir Khan Akash <4175.akash@gmail.com>
2022-09-12 17:06:08 +0600

    Time: 4 ms (56.54%), Space: 5.8 MB (73.88%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -2,6 +2,17 @@ class Solution {
 public:
     int mySqrt(int x) {
 
-        return sqrt(x);
+        long long low=0, high=x,mid;
+        if(x<2)
+            return x;
+        while(low<high)
+        {
+            mid = low + (high - low + 1)/2;
+            if(mid*mid <= (long long)x)
+                low = mid;
+            else
+                high = mid - 1;
+        }
+        return high;
     }
 };
