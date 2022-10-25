887410b544857d2ec9be495c789215142af48f2c
Sabir Khan Akash <4175.akash@gmail.com>
2022-10-25 17:32:32 +0600

    Time: 0 ms (100.00%), Space: 5.9 MB (89.21%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -1,22 +1,11 @@
 class Solution {
 public:
     string defangIPaddr(string address) {
-        int cnt=0;
         for(int i=0; i<address.size(); i++)
-        {
-            if(address[i] == '.')
-            {
-                address.insert(i+1,"]");
-            }
-        }
+            if(address[i] == '.') address.insert(i+1,"]");
         
         for(int j=address.size()-1; j>=0; j--)
-        {
-            if(address[j] == '.')
-            {
-                address.insert(j,"[");
-            }
-        }
+            if(address[j] == '.') address.insert(j,"[");
         
         return address;
     }
