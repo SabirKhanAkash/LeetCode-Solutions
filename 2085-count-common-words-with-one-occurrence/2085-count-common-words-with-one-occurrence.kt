f9e2dac665c4297ff6a4cd783595589489808f89
Sabir Khan Akash <sabir.akash.cs@gmail.com>
2023-10-08 22:02:46 +0600

    Time: 307 ms (11.11%), Space: 38 MB (44.44%) - LeetHub

--- 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.kt
+++ 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.kt
@@ -4,21 +4,17 @@ class Solution {
         var secondWordMap: HashMap<String, Int> = HashMap()
         
         for(i in 0..words1.size-1) {
-            if(firstWordMap.containsKey(words1[i])) {
+            if(firstWordMap.containsKey(words1[i]))
                 firstWordMap.put(words1[i], firstWordMap[words1[i]]!! + 1)
-            }
-            else {
-                firstWordMap.put(words1[i], 1)
-            }    
+            else
+                firstWordMap.put(words1[i], 1)   
         }
         
         for(i in 0..words2.size-1) {
-            if(secondWordMap.containsKey(words2[i])) {
+            if(secondWordMap.containsKey(words2[i]))
                 secondWordMap.put(words2[i], secondWordMap[words2[i]]!! + 1)
-            }
-            else {
-                secondWordMap.put(words2[i], 1)
-            }    
+            else
+                secondWordMap.put(words2[i], 1)    
         }
         var count = 0
         for(i in firstWordMap) {
