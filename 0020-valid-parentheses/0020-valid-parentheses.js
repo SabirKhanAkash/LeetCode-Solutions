47c4e174ea8d3646361a70df721b9e9ee71b7ad2
Sabir Khan Akash <sabir.akash.cs@gmail.com>
2024-09-07 00:38:49 +0600

    Time: 59 ms (48.13%), Space: 49.8 MB (72.52%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+    let stack = [];
+    for (i of s) {
+        if (i == '(' || i == '{' || i == '[') {
+            stack.push(i);
+        }
+        else {
+            if (!stack?.length ||
+                (i == ')' && stack[stack?.length - 1] !== '(') ||
+                (i == '}' && stack[stack?.length - 1] !== '{') ||
+                (i == ']' && stack[stack?.length - 1] !== '[')) {
+                return false;
+            }
+            stack.pop();
+        }
+    }
+    return !stack?.length;
+};
